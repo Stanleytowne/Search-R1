189564ffceb5ae3d9ed65cc90045ae2a56dd4f14
Stanleytowne <3065178273@qq.com>
2025-12-16 18:52:39 +0800

    add printing

--- scripts/process_toolbench_data.sh
+++ scripts/process_toolbench_data.sh
@@ -1,10 +1,16 @@
+echo "[1] Collecting all APIs information and organize them into categories"
+
 python toolbench_data_process/get_all_api_infos.py \
     --input ../StableToolBench/data/instruction/G1_query.json
 
 python toolbench_data_process/get_all_api_infos.py \
     --input ../StableToolBench/data/instruction/G2_query.json
 
+echo "[2] Generating data for knowledge injection"
+
 python toolbench_data_process/gen_stage1_data.py
+
+echo "[3] Generating data for rl format warm-starting"
 
 python toolbench_data_process/gen_stage2_data.py \
     --input ../StableToolBench/data/answer/G1_answer \
@@ -16,7 +22,11 @@ python toolbench_data_process/gen_stage2_data.py \
     --mapping ../StableToolBench/data/instruction/G2_query.json \
     --output ./data/toolbench_stage2
 
+echo "[4] Organizing instruction data for rl training"
+
 python toolbench_data_process/organize_instruction.py
+
+echo "[5] Generating rl data for category Sports"
 
 python toolbench_data_process/gen_rl_data.py \
     --input ./data/toolbench_instruction/Sports.json \
